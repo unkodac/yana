419c2ebbfa0565e6eeb60286209c5674870ee339
unkodac <101387773+unkodac@users.noreply.github.com>
2022-05-11 00:00:55 +0900

    Update log4.js

--- l-tike/touken/log4.js
+++ l-tike/touken/log4.js
@@ -73,5 +73,5 @@ link.download = text_name;link.click();
 
 //次画面へ
 setTimeout(function(){
-//document.querySelector("[name=ENTRY_FIX]").click();
+document.querySelector("[name=ENTRY_FIX]").click();
 },2000); //iPhoneで応募の場合ファイル保存のポップアップによって次へ操作が無効化されるため2秒遅延
